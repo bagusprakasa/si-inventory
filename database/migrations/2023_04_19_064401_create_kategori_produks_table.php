2fd65a8614ff254dd7a26a658b2e086d6863b010
radh3t <131231504+Radit123545@users.noreply.github.com>
2023-04-19 14:07:02 +0700

    add kategory produk table

--- database/migrations/2023_04_19_064401_create_kategori_produks_table.php
+++ database/migrations/2023_04_19_064401_create_kategori_produks_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('kategori_produks', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
