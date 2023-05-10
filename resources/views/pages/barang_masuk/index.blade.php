01dbc0b65f6e00c824db93ff27d493a0a8a65414
radh3t <131231504+Radit123545@users.noreply.github.com>
2023-05-10 14:51:05 +0700

    update barang keluar

--- resources/views/pages/barang_masuk/index.blade.php
+++ resources/views/pages/barang_masuk/index.blade.php
@@ -160,6 +160,23 @@
             })
         }
 
+        function formatRupiah(angka, prefix){
+			var number_string = angka.replace(/[^,\d]/g, '').toString(),
+			split   		= number_string.split(','),
+			sisa     		= split[0].length % 3,
+			rupiah     		= split[0].substr(0, sisa),
+			ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+
+			// tambahkan titik jika yang di input sudah menjadi angka ribuan
+			if(ribuan){
+				separator = sisa ? '.' : '';
+				rupiah += separator + ribuan.join('.');
+			}
+
+			rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+			return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+		}
+
 function detail(id) {
     $('#modal-detail').empty();
     $.ajax({
@@ -174,8 +191,8 @@ function detail(id) {
                     <tr>
                         <td>${v.produk.name}</td>
                         <td>${parseFloat(v.qty)}</td>
-                        <td>${parseFloat(v.subtotal)/parseFloat(v.qty)}</td>
-                        <td>${parseFloat(v.subtotal)}</td>
+                        <td>${formatRupiah((parseFloat(v.subtotal)/parseFloat(v.qty)).toString())}</td>
+                        <td>${formatRupiah(parseFloat(v.subtotal).toString())}</td>
                     </tr>
                     `
                 );
